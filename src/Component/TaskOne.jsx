a58ae856793ad0b39b5ccb7e5ca08cf5e24fe92b
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-10-25 20:46:34 +0000

    I added all the  links button and the footer section. The web view was completed

--- src/Component/TaskOne.jsx
+++ src/Component/TaskOne.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import ProfileImage from "./Images/ProfileImage.png"
+import slack from "./Images/slack.png"
+import github from "./Images/Icon.png"
+import logo from './Images/logo.36d2d48a.png'
+import  lastImage from './Images/I4G.png'
+import shareButton  from "./Images/_Avatar_share.png"
+import InstagramIcon from "./Images/INSTAGRAMICONS.png";
+
 import './TaskOne.css'
 
 const TaskOne =() => {
@@ -26,17 +33,26 @@ const TaskOne =() => {
 							src={ProfileImage}
 							alt=""
 						/>
+						
+						
+						{/* SHARE BUTTON */}
 						<h1 className="twitter" id="Twitter">
-							{" "}
 							Clifftech_Dev
 						</h1>
 						<h1 className="slack" id="slack">
-							{" "}
 							Opoku Isaiah Clifford
 						</h1>
 					</div>
-
-					{/* BUTTON SECTION */}
+					<button type="button" title="share link ">
+						<img
+							className="shareButton"
+							id="shareButton"
+							src={shareButton}
+							alt="share button"
+						/>
+					</button>
+					
+					{/* BUTTON SECTION LINK SECTION  */}
 					<div className="links_section">
 						<button id="btn__zuri" className="btn__zuri">
 							<a
@@ -91,7 +107,26 @@ const TaskOne =() => {
 							</a>
 						</button>
 					</div>
+					{/* SOCIAL  ICONS  SECTION */}
+					<div className="Container ">
+						<div className="content">
+							<div className="icons">
+								<img src={slack} alt="slack" />
+								<img src={github} alt="Github" />
+							</div>
+						</div>
+					</div>
+
+					{/* FOOTER SECTION */}
 				</main>
+				<footer className="footer">
+					<div className="content_Footer">
+						<img className="logo" src={logo} alt="Github" />
+						<h1 className="FooterText">HNG Internship 9 Frontend Task </h1>
+
+						<img className="logo" src={lastImage} alt="Github" />
+					</div>
+				</footer>
 			</div>
 		</>
 	);
